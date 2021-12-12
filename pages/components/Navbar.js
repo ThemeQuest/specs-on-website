867bbe1632e1afc3ee595857bb26f10b27ff0164
StevenPss <pulessteven@gmail.com>
2021-12-12 22:27:58 +0200

    styles :lipstick:: added component navbar

---
+++ pages/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+function Navbar() {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    return (
+        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-6 rounded dark:bg-gray-800 border-b">
+            <div className="container mx-auto flex flex-wrap items-center justify-between">
+            <a href="#" className="flex">
+                {/* add svg here */}
+                <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">SpecsOn</span>
+            </a>
+            <div className="flex md:order-2">
+                <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+                <button data-collapse-toggle="mobile-menu-4" type="button" className="md:hidden text-gray-500 hover:bg-gray-100focus:outline-none focus:ring-2 focus:ring-gray-200 rounded-lg text-sm p-2 inline-flex items-center dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-4" aria-expanded="false"
+                onClick={() => setNavbarOpen(!navbarOpen)}>
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+                    <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                </button>
+            </div>
+                <div className={"md:flex justify-between items-center w-full md:w-auto md:order-1" + (navbarOpen ? "md:flex" : " hidden")}
+                id="mobile-menu-4">
+                    <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+                        <li>
+                            <a href="#" className="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded dark:text-white" aria-current="page">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
+                        </li>
+                        <li>
+                            <a href="#" className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
 import Head from 'next/head'
+import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>SpecsOn</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      Main content
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        Footer
-      </footer>
-    </div>
+    <>
+      <Navbar/>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <Head>
+          <title>SpecsOn</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        Content
+        <footer className="flex items-center justify-center w-full h-24 border-t">
+          Footer
+        </footer>
+      </div>
+    </>
   )
 }
