09fb1fb58a75361da5240160a8a384edafc7d184
StevenPss <pulessteven@gmail.com>
2021-12-18 19:59:54 +0200

    feat :sparkles:: added component featured with video play

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Navbar from './components/Navbar'
 import Banner from './components/Banner';
 import Footer from './components/footer';
+import Featured from './components/Featured';
 
 export default function Home() {
 
@@ -12,8 +13,8 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar/>
-      <Banner/>  
-        Content
+      <Banner/>
+      <Featured/>
       <Footer />
     </>
   )
